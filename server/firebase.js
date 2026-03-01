d329afe317139e4c10e5fce9a18cf1bddfbf1a10
Fiza Pathan <fizapathan2102@gmail.com>
2026-03-01 11:28:56 -0500

    Initialize Firebase Admin SDK and Firestore

---
+++ server/firebase.js
@@ -0,0 +1,14 @@
+const admin = require('firebase-admin');
+const path = require('path');
+
+// Load service account key
+const serviceAccount = require(path.join(__dirname, 'serviceAccountKey.json'));
+
+// Initialize Firebase Admin
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+module.exports = db;
